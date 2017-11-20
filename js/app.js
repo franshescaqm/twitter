a2d729af69c030c3fcf8b5bb932c8e9295b65076
franshescaqm <franshesca_94@hotmail.com>
2017-11-20 10:56:56 -0500

    HTML Y CSS Terminado. Empezando con JS

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function(event) {
+  var tweetContainer = document.getElementById('tweeting');
+  var textingArea = tweetContainer.firstElementChild;
+  var boton = document.getElementById('boton');
+  textingArea.addEventListener('click', function() {
+    if (textingArea === textContent.value)
+      var newElementContainer = document.createElement('div');
+    var newTextContainer = newElementContainer.createTextNode('');
+  });
+});
